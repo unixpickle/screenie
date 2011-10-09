7f4b27817aef676e29464e131c749a2e27c09ac2
Alex Nichol <alexnichol@comcast.net>
2011-10-09 09:44:17 -0400

    Initial Commit

---
+++ screenie/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  screenie
+//
+//  Created by Alex Nichol on 10/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+
+	// insert code here...
+	NSLog(@"Hello, World!");
+
+	[pool drain];
+    return 0;
+}
+
